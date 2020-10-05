f9143d9ccf324732f5391e6e7837ca99f798f23f
Vincent Dechenaux <vincent.dechenaux@gmail.com>
2020-10-05 21:34:36 +0200

    Statically build the binary

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,6 @@ cd /tmp
 wget https://github.com/google/brotli/archive/v$VERSION.zip
 unzip v$VERSION.zip
 cd brotli-$VERSION
-make -j $(nproc) brotli
+LDFLAGS=-static CFLAGS=-static make -j $(nproc) brotli
 strip bin/brotli
 cp bin/brotli /output/brotli-amd64
